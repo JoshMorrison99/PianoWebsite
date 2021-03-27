ce6a41f8da5d6a37e9cd8885cda8883fff0e5f00
Josh Morrison <josh.morrison1999@gmail.com>
2021-03-27 12:01:29 -0400

    created an about me page

--- src/About.js
+++ src/About.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import * as ReactBootStrap from "react-bootstrap";
+import me from './images/me.jpg';
+import './css/About.css';
 
 function About(){
     return(
-        <div>
-            <h1>About</h1>
-        </div>
+        <ReactBootStrap.Container>
+            <ReactBootStrap.Row>
+                <ReactBootStrap.Col>
+                    <div className="textBox">
+                        <p>&nbsp;&nbsp;&nbsp;&nbsp; Hi, my names Josh Morrison, I'm a third year computer science student at Carleton University. 
+                            I created this piano application because I suck at piano and wanted a fun way to learn how to play.</p>
+
+                        <p>&nbsp;&nbsp;&nbsp;&nbsp; I am currently the dumbest person I know, so there is indeed some problems wrong with the application. If you find any problems that arise when playing,
+                            please email me <u>dawdio_business@gmail.com</u> with the error you encountered.</p>
+
+                        <p>&nbsp;&nbsp;&nbsp;&nbsp; If you enjoy the application, you can support server costs here: <a href="https://www.patreon.com/dawdio"> https://www.patreon.com/dawdio </a></p>
+                    </div>
+                </ReactBootStrap.Col>
+                <ReactBootStrap.Col>
+                    <div className="pictureDiv">
+                        <img className="pic" src={me} height="514" width="386"/>
+                    </div>
+                </ReactBootStrap.Col>
+            </ReactBootStrap.Row>
+        </ReactBootStrap.Container>
     )
 }
 
