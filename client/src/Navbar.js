b2860b882f06d1ae0653b68dae64a54bdf48fe61
Josh Morrison <josh.morrison1999@gmail.com>
2021-05-17 21:06:11 -0400

    update

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as ReactBootStrap from "react-bootstrap";
 import './css/Navbar.css';
-import setup from "./setup/mysetup.exe";
+import setup from "./setup/PrimePianistInstaller.exe";
 
 function Navbar(){
     return (
