4bd1499ce96b4cbd44f95cc49d4ed2e9ce4ca633
Josh Morrison <josh.morrison1999@gmail.com>
2021-05-23 22:21:25 -0400

    added ability for the server to communicate with game for account creation

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
 const { isEmail } = require('validator')
+const bcrypt = require('bcrypt');
 
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: [true, 'Please enter a username'],
+        minlength: [4, 'Minimum username length is 4 characters'],
         unique: true
     },
     email: {
@@ -20,8 +22,14 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Please enter a password'],
         minlength: [6, 'Minimum password length is 6 characters']
     }
-
 });
+
+userSchema.pre('save', async function (next) {
+    console.log('user about to be created and saved', this);
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next()
+})
 
 const User = mongoose.model('user', userSchema)
 
