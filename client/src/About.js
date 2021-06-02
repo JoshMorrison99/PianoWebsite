a492262143d6febf134512b67cd0540ce977e8c0
Josh Morrison <josh.morrison1999@gmail.com>
2021-06-02 12:43:20 -0400

    change

--- client/src/About.js
+++ client/src/About.js
@@ -16,8 +16,8 @@ function About(){
                             please email me <b>primepianist@gmail.com</b> with the error you encountered.</p>
                         
                         <p>&nbsp;&nbsp;&nbsp;&nbsp; To be in the loop on the what's coming next to the application,
-                            I will be posting on this website, as well as doing updates on <a target="a_blank" href="https://www.youtube.com/channel/UCDj-K11fBHMZB7w31T3X08A">YouTube</a>, 
-                            <a target="a_blank" href="https://twitter.com/PianistPrime"> Twitter</a>,  <a target="a_blank" href="https://discord.gg/EuesAp9c"> Discord</a> and <a target="a_blank" href="https://www.instagram.com/primepianist/">Instagram</a>.  </p>
+                            I will be posting on this website, as well as doing updates on <a target="a_blank" href="https://www.youtube.com/channel/UCXT-b6QjIBcMUNaEic7dTTw">YouTube</a>.
+                            Follow me on <a target="a_blank" href="https://www.instagram.com/joshmorrison99/">Instagram</a> too.  </p>
                              
                         <p>&nbsp;&nbsp;&nbsp;&nbsp;If you have any ideas of features to add to the application, you can do so by contacting me on anything previously mentioned. </p>
 
