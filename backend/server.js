e8a4682996c3c4911db2ba0f66177942411e007f
Josh Morrison <josh.morrison1999@gmail.com>
2021-06-08 23:29:02 -0400

    added game launcher back

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,18 @@ const app = express();
 
 const port = process.env.PORT || 5000
 
+app.get('/api/game_version', (req, res) => {
+    fs.readFile('./Game/game_version.txt', (err, data) => {
+        res.send(data)
+    })
+})
+
+app.get('/api/game_download', (req, res) => {
+    fs.readFile('./Game/download_game.zip', (err, data) => {
+        res.setHeader('content-type', 'application/octet-stream');
+        res.send(data)
+    })
+})
 
 // Server Listen on port
 app.listen(5000);
